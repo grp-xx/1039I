3050e1bc1da809030a046535a14e1ac52859ca38
Gregorio <gregorio.procissi@gmail.com>
2022-05-03 23:07:08 +0200

    added #who, #bye and #pm commands

--- src/UDProom-server.cpp
+++ src/UDProom-server.cpp
@@ -48,117 +48,110 @@ int main()
 
         
 
-        switch (msg.type) 
+        if (msg.code == "#join") 
         {
-            case msg_type::C :
+            // send response to join message
+            message response = {
+                .type = msg_type::C,
+                .to   = msg.from,
+            };
 
-                if (msg.code == "#join") {
-                    // send response to join message
-                    message response = {
-                        .type = msg_type::C,
-                        .to   = msg.from,
-                    };
+            if (cmap.find(msg.from) == cmap.end())  // User with new identifier 
+            {
+                cmap[msg.from] = client;
+                response.code  = "OK";
+                response.text  = "Welcome to the NPL chat room!!";
 
-                    if (cmap.find(msg.from) == cmap.end())  // User with new identifier 
-                    {
-                        cmap[msg.from] = client;
-                        response.code  = "OK";
-                        response.text  = "Welcome to the NPL chat room!!";
+                msg_send(response, client);
+                // Send welcome message to connecting user                
 
-                        // format response to json
-                        json response_j = response;
-                        std::string response_str = response_j.dump();
 
-                        // Send welcome message to connecting user                
-                        sock.sendto(npl::buffer(response_str.begin(),response_str.end()), client);
 
-                        // Notify all other users of new arrival
+                // Prepare message
+                message to_all {
+                    .type = msg_type::D,
+                    .code = "",
+                    .to   = "all",
+                    .from = "Server",
+                    .text = msg.from + " has joined the chatroom",
+                };
 
-                        // Prepare message
-                        message to_all {
-                            .type = msg_type::D,
-                            .code = "",
-                            .to   = "all",
-                            .from = "Server",
-                            .text = msg.from + " has joined the chatroom",
-                        };
-
-                        // Format response to json format
-
-                        json to_all_j = to_all;
-                        std::string to_all_str = to_all_j.dump();
-                        std::cout << to_all.text << std::endl;  // Output to stdout the new joining user
+                std::cout << to_all.text << std::endl;  // Output to stdout the new joining user
                         
-                        // Notify all but the new arrived
-                        for (auto [k,dest] : cmap) 
-                        {
-                            if (k != msg.from) 
-                            {
-                                sock.sendto(npl::buffer(to_all_str.begin(),to_all_str.end()), dest);
-                            }
-                        }                                       
-                    }
-
-                    else // There was already a user with the same user name 
-                    {
-                        // Prepare message of reject
-                        response.code = "NO";
-                        response.text = "Access refused, existing username";
-                        json response_j = response;
-                        std::string response_str = response_j.dump();
-
-                        // Send welcome message to connecting user                
-                        sock.sendto(npl::buffer(response_str.begin(),response_str.end()), client);
-                    }
-                    
-                }
-
-                if ( (msg.code == "#bye") || (msg.code == "#leave") ) 
-                {
-                    cmap.erase(msg.from);  //Remove client key from the map
-                    std::cout << msg.from << " has left the chat room" <<std::endl; // Printo to screen locally on server
-                    // Send bye to the leaving user
-                    message mbye = {msg_type::D, "#byeOK", msg.from, "Server", "Bye bye " + msg.from};
-                    msg_send(mbye, client);
-                    // Notify all that the user has left the room
-                    message mbyeall = {msg_type::D, "", "all", "Server", msg.from + " has left the chat room"};
-                    for (auto [k,dest] : cmap) 
-                    {
-                        msg_send(mbyeall, dest);
-                    }                                        
-                }
-
-                if (msg.code == "#who") {
-                    std::stringstream ss;
-                    for (auto i : cmap) 
-                        ss << i.first << ", ";
-                    
-                    std::string list = ss.str();
-                    message response = {msg_type::D, "OK", msg.from, "Server", "Users in the room: " + std::string(list.begin(),list.end()-2)};  // remove last two charcters ", "
-                    msg_send(response, client);
-                    // json response_j = response;
-                    // std::string response_str = response_j.dump();
-                    // Send list of usres to requesting user                
-                    // sock.sendto(npl::buffer(response_str.begin(),response_str.end()), client);
-                }
-                break;
-
-            default:
-                // Broadcast text message to all users partecipating the chat 
-                for (auto [k,dst] : cmap) 
+                // Notify all but the new arrived
+                for (auto [k,dest] : cmap) 
                 {
                     if (k != msg.from) 
                     {
-                        sock.sendto(buf, dst);
+                        msg_send(to_all, dest);
                     }
-                }
-                break;
+                }                                       
 
             }
+            else // There was already a user with the same user name 
+            {
+                // Prepare message of reject
+                response.code = "NO";
+                response.text = "Access refused, existing username";
+                // Send welcome message to connecting user 
+                msg_send(response, client); 
+            }
+            continue;            
+        }
+
+        if ( (msg.code == "#bye") || (msg.code == "#leave") ) 
+        {
+            cmap.erase(msg.from);  //Remove client key from the map
+            std::cout << msg.from << " has left the chat room" <<std::endl; // Printo to screen locally on server
+            // Send bye to the leaving user
+            message mbye = {msg_type::D, "#byeOK", msg.from, "Server", "Bye bye " + msg.from};
+            msg_send(mbye, client);
+            // Notify all that the user has left the room
+            message mbyeall = {msg_type::D, "", "all", "Server", msg.from + " has left the chat room"};
+            for (auto [k,dest] : cmap) 
+            {
+                msg_send(mbyeall, dest);
+            }
+            continue;                                       
+        }
+
+        if (msg.code == "#who") 
+        {
+            std::stringstream ss;
+            for (auto i : cmap) 
+                ss << i.first << ", ";
+            
+            std::string list = ss.str();
+            message response = {msg_type::D, "OK", msg.from, "Server", "Users in the room: " + std::string(list.begin(),list.end()-2)};  // remove last two charcters ", "
+            msg_send(response, client);
+            // Send list of users to requesting user                
+            continue;
+        }
+
+        if (msg.code == "#pm")
+        {
+            if (cmap.find(msg.to) == cmap.end())  // User with new identifier 
+            {
+                message response = {msg_type::C, "#pmNO", msg.from, "Server", "User unknown"};
+                msg_send(response, client);
+            }
+            else 
+            {
+                msg_send(msg, cmap[msg.to]);
+            }
+            continue;
+        }
+            
+        // default behavior   
+        // Broadcast text message to all users partecipating the chat 
+        for (auto [k,dst] : cmap) 
+        {
+            if (k != msg.from) 
+            {
+                sock.sendto(buf, dst);
+            }
+        }
     }
-
-
-
 
     return 0;
 }
--- src/UDProom.cpp
+++ src/UDProom.cpp
@@ -44,9 +44,9 @@ void transmitter(const npl::sockaddress<AF_INET>& srv_addr, const std::string& u
         m.unlock();
         std::string line;
         std::getline(std::cin,line);
-        std::stringstream ss(line);   // Convert input line into a string stream (ingnore white spaces)
-        std::string first, second;
-        ss >> first >> second;        // retrieve first and second words (ignore white spaces)
+        std::stringstream ss(line);            // Convert input line into a string stream (ingnore white spaces)
+        std::string first, second, third;
+        ss >> first;                           // retrieve first word (ignore white spaces)
 
         if (first == "#who") 
         {
@@ -62,6 +62,17 @@ void transmitter(const npl::sockaddress<AF_INET>& srv_addr, const std::string& u
             continue;
         }
 
+        if (first == "#pm") 
+        {
+            ss >> second >> third;             // extract second and third words (third word only to remove initial blanks in the text)
+            std::string remaining;
+            std::getline(ss, remaining);       // taking the remaining string line out of stream ss after the first, second and third words have been extracted. Extract the third to remove initial blanks. 
+            message msg {msg_type::C, "#pm", second, user, third + remaining};
+            msg_send(msg, srv_addr);
+            continue;
+        }
+
+        // Default behavior
         message msg {msg_type::D, "", "all", user, line};
         msg_send(msg, srv_addr);
     }
@@ -73,13 +84,10 @@ void receiver(const std::string& user)
     for(;;)
     {
         message msg = msg_rcv();
-        // auto [buf, srv] = sock.recvfrom(360);
-        // auto msg_str = std::string(buf.begin(),buf.end());
-        // json msg_j = json::parse(msg_str);
-        // message msg = msg_j;
 
         m.lock();
-        std::cout << std::endl << "<" << msg.from << ">" << ": " << msg.text << std::endl;
+        std::string prompt = (msg.code == "#pm" ? msg.from + " (PM)" : msg.from);
+        std::cout << std::endl << "<" << prompt << ">" << ": " << msg.text << std::endl;
         m.unlock();
         if (msg.code == "#byeOK") {
             exit(0);
@@ -87,7 +95,7 @@ void receiver(const std::string& user)
         m.lock();
         std::cout << "<You>: " << std::flush;
         m.unlock(); 
-
+    
     }
 }
 
@@ -119,19 +127,10 @@ int main(int argc, char** argv)
         };       
 
         msg_send(mjoin, srv_addr);
+
         // Receive response...
-
-        // auto [buf, srv] = sock.recvfrom(360);
-        // auto response_str = std::string(buf.begin(),buf.end());
-        // json response_j   = json::parse(response_str);
-
-        // Print initial message (welcome/refuse)
-        // std::cout << response_j.at("text").get<std::string>() << std::endl;
-        // if ( response_j.at("code").get<std::string>() == "OK" ) 
-        //    break ;
-
         message response = msg_rcv();
-        
+        // Print Welcome/Refuse message from the system        
         std::cout << response.text << std::endl;
 
         if (response.code == "OK") 
