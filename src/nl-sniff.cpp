e3aa23bc6eb43cd98e330987767a825458351a30
Gregorio <gregorio.procissi@gmail.com>
2022-05-16 11:02:02 +0200

    minor

--- src/nl-sniff.cpp
+++ src/nl-sniff.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <netinet/in.h>
 #include <sys/socket.h>
 #include "socket.hpp"
 #include "sockaddress.hpp"
@@ -9,19 +10,20 @@
 
 int main(int argc, char** argv)
 {
-    npl::socket<AF_INET, SOCK_RAW> sock(ntohs(ETHERTYPE_IP));
+    npl::socket<AF_INET, SOCK_RAW> sock(IPPROTO_TCP);
 
 
     for (;;)
     {
-        npl::buffer buf = sock.read(65536);
+        auto [buf, from] = sock.recvfrom(65536);
 
 
         //const struct ip* ip_ptr = reinterpret_cast<const ip*>(&buf[0]);
 
         npl::pktheader<ipv4> ip_hdr(&buf[0],buf.size());
         std::cout << "Header length: " << ip_hdr.hlen() << " ";
-        std::cout << "From " << ip_hdr.src() << " To: " << ip_hdr.dst() << std::endl;
+        // std::cout << "From " << ip_hdr.src() << " To: " << ip_hdr.dst() << std::endl;
+        std::cout << "From: " << from.host() << " Port: " << from.port() << std::endl;
         
 
 
