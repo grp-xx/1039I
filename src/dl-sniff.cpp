d87ed5e892534b4b7051c7cacf4e5f5fcca24c83
Gregorio <gregorio.procissi@unipi.it>
2022-05-17 16:42:35 +0200

    minor

--- src/dl-sniff.cpp
+++ src/dl-sniff.cpp
@@ -3,18 +3,20 @@
 #include <net/ethernet.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
+#include "frame.hpp"
 #include "socket.hpp"
 #include "sockaddress.hpp"
 #include <netinet/ip.h>
 #include <thread>
 #include <vector>
 #include "headers.hpp"
+#include "frame.hpp"
 
 
 void frame_capture(std::string iface, unsigned short protocol)
 {
     npl::sockaddress<AF_PACKET> device(iface, protocol);
-    npl::socket<AF_PACKET, SOCK_DGRAM> sock;
+    npl::socket<AF_PACKET, SOCK_RAW> sock;
     sock.bind(device);
     sock.set_promisc(iface);
     sock.set_fanout(42);
@@ -22,9 +24,15 @@ void frame_capture(std::string iface, unsigned short protocol)
     for (;;)
     {
         auto [buf, from] = sock.recvfrom(65536);
-        npl::pktheader<ipv4> ip_hdr(&buf[0],buf.size());
-        std::cout << "Thread :" << std::this_thread::get_id() << " "; 
-        std::cout << "From " << ip_hdr.src() << " To: " << ip_hdr.dst() << std::endl;
+        npl::frame ff(&buf[0],buf.size());
+
+        if (ff.is<ipv4>()) 
+        {
+            std::cout << "From: " << ff.header<ipv4>().src() << " --> To: " << ff.header<ipv4>().dst() << std::endl;
+            std::cout << "Vlan ID: " << ff.header<vlan>().vlan_id() << std::endl;
+
+        } 
+
     }
 
     sock.close();
@@ -38,7 +46,7 @@ int main(int argc, char** argv)
     std::vector<std::thread> thread_pool;
 
     for (auto i = 1; i <= N_thread; i++) {
-        std::thread t(frame_capture, "ens33", ETHERTYPE_IP);
+        std::thread t(frame_capture, "ens33", ETH_P_ALL);
         thread_pool.push_back(std::move(t));
     }
 
